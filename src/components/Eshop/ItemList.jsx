56a483c0b4e9ba1ef6db2201d13490b8e4a1ff25
callbackKid <vladislav.yarosh23@gmail.com>
2023-06-20 12:46:05 +0400

    created post request

--- src/components/Eshop/ItemList.jsx
+++ src/components/Eshop/ItemList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Item } from './Item'
 import { AddItem } from './AddItem'
 import classes from './itemList.module.css'
+import { postItem } from '../../requests'
 
 export const ItemList = () => {
   const [items, setItems] = useState([])
@@ -26,7 +27,8 @@ export const ItemList = () => {
   }, [])
 
   const handlePostedData = async (obj) => {
-    setItems([...items, obj])
+    const responseItem = await postItem(obj)
+    setItems([...items, responseItem])
   }
 
   return (
---
+++ src/requests.js
@@ -0,0 +1,24 @@
+export const postItem = async (item) => {
+  const url = 'https://api.escuelajs.co/api/v1/products'
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(item),
+    })
+
+    if (!response.ok) {
+      throw new Error('request failed')
+    }
+
+    console.log(response)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
