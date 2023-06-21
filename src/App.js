b4df3dfa652bb32d88b79fc107e35d0193fe153a
Anahit.Nalbandyan <anahitgrigoryanr@gmail.com>
2023-06-21 16:46:17 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
 import { ItemList } from './components/Eshop/ItemList'
+
+
+import { ApiContext } from './components/context'
+import { useState,useEffect } from 'react'
+
+
+import { AddItem } from './components/Eshop/AddItem'
+import { postItem, deleteItem } from './request'
+
 import classes from './app.module.css'
+import axios from 'axios'
 
 function App() {
+
+  const [items, setItems]= useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    const getdata = async () => {
+      setIsLoading(true)
+      try {
+        const {data} = await axios.get('https://api.escuelajs.co/api/v1/products?limit=7&offset=1')
+        
+        setItems(data)
+        setIsLoading(false)
+      } catch (error) {
+        console.error('Error:', error)
+        setError(error)
+        setIsLoading(false)
+      }
+    }
+    getdata()
+  }, [])
+
+  
+  const handlePostedData = async (obj) => {
+  const responseItem =  await postItem(obj)
+  setItems([...items, responseItem])
+  }
+
+  const handleDeleteItem = async(id)=>{
+    const response = await deleteItem(id)
+    response 
+      ? setItems((prevItems)=>prevItems.filter((el)=> el.id !==id))
+      : console.log('error')
+  }
+
   return (
-    <div className="App">
-      <h1 className={classes.header}>Our Best Eshop</h1>
-      <ItemList />
-    </div>
+  
+    <ApiContext.Provider value={{items, handlePostedData, handleDeleteItem}}>
+
+      <div className="App">
+      <div className={classes.addItemContainer}>
+        <AddItem/>
+      </div>
+        <h1 className={classes.header}>Our Best Eshop</h1>
+      { error ? <h1>An error occurred: {error.message}</h1> : null}
+        {isLoading ? <h1>LOADING...</h1>: 
+        <ItemList />}
+      </div> 
+
+    
+
+  </ApiContext.Provider>
   )
 }
 
--- src/components/Eshop/Item.jsx
+++ src/components/Eshop/Item.jsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react'
+import { ApiContext } from '../context'
 import classes from './item.module.css'
 
-export const Item = ({ title, price, image, description }) => {
+export const Item = ({ id, title, price, image, description }) => {
+  
+  const {handleDeleteItem } = useContext(ApiContext)
+  
   return (
     <div className={classes.item}>
       <h1>{title}</h1>
       <img className={classes.image} src={image} alt={title} />
       <p className={classes.description}>{description}</p>
       <span>{price}$</span>
+      <button onClick={()=>handleDeleteItem(id)}>Delete</button>
     </div>
   )
 }
