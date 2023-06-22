a72b0f4408276b1c16f1df1684c456d61ccbe5c3
Anahit.Nalbandyan <anahitgrigoryanr@gmail.com>
2023-06-22 07:11:25 +0200

    New commit

--- src/components/Eshop/Item.jsx
+++ src/components/Eshop/Item.jsx
@@ -5,10 +5,10 @@ import classes from './item.module.css'
 export const Item = ({ id, title, price, image, description }) => {
   
   const {handleDeleteItem } = useContext(ApiContext)
-  
+  const {theme} = useContext(ApiContext)
 
   return (
-    <div className={classes.item}>
+    <div className={`${classes.item} ${theme}`}>
       <h1>{title}</h1>
       <img className={classes.image} src={image} alt={title} />
       <p className={classes.description}>{description}</p>
